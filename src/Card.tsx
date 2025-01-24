cd8b2558bee9892730de6ed011e2f6751d799aa0
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2025-01-25 00:03:30 +0300

    Init store on client and add styled-components

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
+import { styled } from 'styled-components';
+
+const $Card = styled.div`
+      color: red ;    
+    `;
 
 function Card() {
+
+
   const [count, setCount] = useState(0)
 
   return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
+    <$Card>
+    <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
       </p>
-    </div>
+    </$Card>
   )
 }
 
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,11 +1,16 @@
 import './index.css'
-import { StrictMode } from 'react'
-import { hydrateRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {hydrateRoot} from 'react-dom/client'
 import App from './App'
+import {Provider} from 'react-redux';
+import {store} from '@/store/store';
+
 
 hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    document.getElementById('root') as HTMLElement,
+    <StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </StrictMode>,
 )
